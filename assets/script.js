1680dcae8337093578914c5fc80405c1d35055ad
Michael Aiello <mjahere023@gmail.com>
2020-01-16 21:30:11 -0700

    Finished first ajax call into open weather map.

--- assets/script.js
+++ assets/script.js
@@ -8,9 +8,9 @@ function storeCities() {
 }
 
 // adds last searched as button
-function createCityList(){
+function createCityList() {
     $(".cityList").empty();
-    cityList.forEach(function(city){
+    cityList.forEach(function (city) {
         $(".cityList").prepend($(`<button class="list-group-item list-group-item-action cityButton" data-city="${city}">${city}</button>`));
     })
 }
@@ -30,8 +30,29 @@ function init() {
         getForecast(thisCity, id);
     }
 }
-
+//retrieving current weather forecast for user city selection
 function getCurrentWeather(thisCity, id) {
     var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${thisCity}&units=imperial&appid=${id}`;
+    var cityLat;
+    var cityLong;
+
+    $.ajax({
+        url: weatherURL,
+        method: "GET"
+    }).then(function (data) {
+        $(".cityToday").append(
+            `<div class="row ml-1">
+                <h3 class="mr-3">${data.name} (${(new Date(1000 * data.dt).getUTCMonth()) + 1}/${(new Date(1000 * data.dt).getUTCDate()) - 1}/${new Date(1000 * data.dt).getUTCFullYear()})</h3>
+                <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
+            </div>`
+        )
+        $(".cityToday").append(`<p>Temperature: ${data.main.temp} &degF</p>`)
+        $(".cityToday").append(`<p>Humidity: ${data.main.humidity} %</p>`)
+        $(".cityToday").append(`<p>Wind: ${data.wind.speed} mph</p>`)
+        cityLat = data.coord.lat;
+        cityLong = data.coord.lon;
+        getUVI(id, cityLat, cityLong);
+    })
+
 }
 
